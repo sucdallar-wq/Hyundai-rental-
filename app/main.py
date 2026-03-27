3fd54f90674ad25ce4e16994310ed0a8a6a8bc9d
Soner Üçdallar <s.ucdallar@tetas.com.tr>
2026-03-27 17:02:08 +0300

    cors final

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException, File, UploadFile, Body
 from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
-origins = [
-    "http://localhost:5173",   
-    "http://127.0.0.1:5173",
-]   
 
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # 🔥 FULL OPEN
+    allow_origins = [
+        "http://localhost:5173",   
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
